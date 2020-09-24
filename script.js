e3cece14741c49e4a2c746b9cce122708ceed220
kramlex <mark.dubkov@gmail.com>
2020-09-25 03:06:13 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const red = '#ff4040'
+const defaultColor = document.getElementsByClassName('td')[0].style.backgroundColor
+
+
+function findElem(){
+    let title = document.getElementById('search-text').value.toLowerCase()
+
+    let titleDivs = [...[...document.getElementsByClassName('td')]
+        .filter(elem => elem.dataset.label==='Автор')
+    ]
+    const foundDivs = []
+    const otherDivs = []
+    titleDivs.forEach(elem => {
+        elem.style.backgroundColor = defaultColor
+        elem.innerText.toLowerCase().indexOf(title) === -1 ? otherDivs.push(elem) : foundDivs.push(elem)
+    })
+    console.log(foundDivs, otherDivs)
+
+    otherDivs.forEach(elem => {
+        elem.style.backgroundColor = defaultColor
+    })
+    if(!title) return
+    foundDivs.forEach(elem => {
+        elem.style.backgroundColor = red
+    })
+
+}
